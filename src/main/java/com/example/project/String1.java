795e156cabcb1dd2d36165e05618be47605fdc75
mhpolymath <mhpolymath@gmail.com>
2024-09-14 23:17:52 +0300

    blah

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -30,9 +30,31 @@ public class String1 {
             append(s.getChar(i));
     }
     public boolean equal(String1 s) {
-        throw new UnsupportedOperationException("equal() function is not supported yet.");
+        //throw new UnsupportedOperationException("equal() function is not supported yet.");
+        if (s.size != size){
+            return false;
+        }
+        for (int i = 0; i <= size; i++){
+            if (s.str[i] != str[i]){
+                return false;
+            }
+        }
+        return true;
     }
     public void reverse() {
-        throw new UnsupportedOperationException("reverse() function is not supported yet.");
+        //throw new UnsupportedOperationException("reverse() function is not supported yet.");
+        /*
+         *  
+         *for i in str up to half
+         * tmp = str[i]
+         * str[i] = str[size-i]
+         * str [size-i] = tmp
+         */
+
+        for (int i= 0; i <= size/2; i++){
+            char tmp = str[i];
+            str[i] = str[size-i];
+            str [size-i] = tmp;
+        }
     }
 }
